bbd0f56f72607afcc8f3e15bbba02aebb6f968fb
Gil Shechter <gilshechter13@gmail.com>
2022-11-29 11:33:35 +0200

    Creating StudentsRepository class

---
+++ Telhai.CS.Demos/Models/StudentsRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telhai.CS.Demos.Models
+{
+    public class StudentsRepository
+    {
+        private List<Student> _students;
+        public StudentsRepository()
+        {
+            _students = new List<Student>();
+        }
+
+        public Student[] Students
+        {
+            get { return _students.ToArray(); }
+        }
+    }
+}
