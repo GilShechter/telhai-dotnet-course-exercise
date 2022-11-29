98dc7b0c9388addc6532e27f51f387a1b8fba631
Gil Shechter <gilshechter13@gmail.com>
2022-11-29 11:25:00 +0200

    Add project files.

---
+++ Telhai.CS.Demos/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telhai.CS.Demos.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public int Age { get; set; }
+
+        public Student()
+        {
+
+        }
+
+    }
+}
